dfd7c2e633c15dfdb719621a95006c5549f89a91
Mark Jarrell <jarrellmark@gmail.com>
2020-08-19 22:18:36 -0700

    Support Chromium snap package.

--- find-chromium.sh
+++ find-chromium.sh
@@ -21,11 +21,16 @@ if [ -d /usr/lib/chromium ]; then
 elif [ -d /usr/lib/chromium-browser ]; then
 	# Ubuntu
 	CHROMIUM_DIR=/usr/lib/chromium-browser
+elif [ -d $HOME/snap/chromium/current/.local ]; then
+	# Snap
+	mkdir -p $HOME/snap/chromium/current/.local/lib
+	CHROMIUM_DIR=$HOME/snap/chromium/current/.local/lib
 else
 	>&2 echo <<EOF
 Where is lib/ for chromium installed? Couldn't find it on the normal paths like:
 /usr/lib/chromium
 /usr/lib/chromium-browser
+$HOME/snap/chromium/current/local/lib
 
 Edit ./find-chromium.sh and set CHROMIUM_DIR to that directory.
 EOF
