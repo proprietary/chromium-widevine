e8e28f9ff999dd1d4f7bb59ae974a36cca2c148d
amidevous <amidevous@users.noreply.github.com>
2023-05-26 21:51:03 +0200

    Update fetch-latest-widevine.sh

--- fetch-latest-widevine.sh
+++ fetch-latest-widevine.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-# Set ARCH variable
-ARCH=""
-
-[[ -z ${ARCH} ]] && echo "Architecture not supported" 1>&2 && exit 1
-
 # Detect wget/curl
 DLTOOL=""
 which wget && DLTOOL="wget -O -"
 which curl && DLTOOL="curl -L"
 [[ -z ${DLTOOL} ]] && echo "No download tool found on this system" 1>&2 && exit 1
 
-# Begin download
+# Set ARCH variable
+ARCH=""
+
+[[ -z ${ARCH} ]] && echo "Architecture not supported" 1>&2 && exit 1
 case `uname -m` in
 	i?86)
 		ARCH="ia32"
@@ -22,6 +20,8 @@ case `uname -m` in
 		VERSION=$(${DLTOOL} https://dl.google.com/widevine-cdm/versions.txt | tail -n1)
 		;;
 esac
+
+# Begin download
 # Fix download argument for wget
 DARG=${@}
 [[ ${DLTOOL} == "wget -O -" ]] && DARG=`echo ${DARG} | sed 's/\-o /\-O /'` && DLTOOL="wget"
