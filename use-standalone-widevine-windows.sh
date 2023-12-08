2b1d37e3090b3b511139a590c115420c8345c9fc
amidevous <amidevous@users.noreply.github.com>
2023-12-08 12:10:12 +0100

    Update use-standalone-widevine-windows.sh

--- use-standalone-widevine-windows.sh
+++ use-standalone-widevine-windows.sh
@@ -1,31 +1,36 @@
 #!/bin/bash
-CHROMIUM_DIR="$(/bin/sh ./find-chromium.sh)"
+CHROMIUM_DIR="$(/bin/sh ./find-chromium-windows.sh)"
 if [ -z "$CHROMIUM_DIR" ]; then
 	exit 1
 fi
 
 # Get Widevine archive
-/bin/sh ./fetch-latest-widevine.sh -o ./widevine.zip
+/bin/sh ./fetch-latest-widevine-windows.sh -o ./widevine.zip
 # Expected directory structure:
 #	  widevine.zip
 #	  ├── LICENSE.txt
 #	  ├── manifest.json
-#	  └── libwidevinecdm.so
+#	  └── widevinecdm.dll
+#	  └── widevinecdm.dll.sig
+#	  └── widevinecdm.dll.lib
 
 # Extract Widevine and recreate this directory structure under the Chromium directory:
 #	  /usr/lib/chromium/WidevineCdm
 #	  ├── LICENSE
 #	  ├── manifest.json
 #	  └── _platform_specific
-#		  └── linux_x64
-#				└── libwidevinecdm.so
+#		  └── win-x64
+#				└── widevinecdm.dll
+#				└── widevinecdm.dll.sig
+#				└── widevinecdm.dll.lib
 
-sudo mkdir -p "$CHROMIUM_DIR/WidevineCdm/_platform_specific/linux_x64"
-unzip -p widevine.zip LICENSE.txt | sudo dd status=none of="${CHROMIUM_DIR}/WidevineCdm/LICENSE"
-unzip -p widevine.zip manifest.json | sudo dd status=none of="${CHROMIUM_DIR}/WidevineCdm/manifest.json"
-unzip -p widevine.zip libwidevinecdm.so | sudo dd status=none of="${CHROMIUM_DIR}/WidevineCdm/_platform_specific/linux_x64/libwidevinecdm.so"
-find "$CHROMIUM_DIR/WidevineCdm" -type d -exec sudo chmod 0755 '{}' \;
-find "$CHROMIUM_DIR/WidevineCdm" -type f -exec sudo chmod 0644 '{}' \;
-
+mkdir -p "$CHROMIUM_DIR/WidevineCdm/_platform_specific/win-x64/"
+cd "$CHROMIUM_DIR/WidevineCdm/"
+unzip widevine.zip
+mkdir -p "${CHROMIUM_DIR}/WidevineCdm/LICENSE/"
+mv LICENSE.txt "${CHROMIUM_DIR}/WidevineCdm/LICENSE/"
+mv widevinecdm.dll "$CHROMIUM_DIR/WidevineCdm/_platform_specific/win-x64/"
+mv widevinecdm.dll.sig "$CHROMIUM_DIR/WidevineCdm/_platform_specific/win-x64/"
+mv widevinecdm.dll.lib "$CHROMIUM_DIR/WidevineCdm/_platform_specific/win-x64/"
 # clean up
 rm ./widevine.zip
