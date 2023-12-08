4b14a9738ffab69be76d77f71b1637da611fe6ec
amidevous <amidevous@users.noreply.github.com>
2023-12-08 11:54:00 +0100

    Update find-chromium-windows.sh

--- find-chromium-windows.sh
+++ find-chromium-windows.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+if [ -d "/cygdrive/c/Program Files/chromium" ]; then
+	CHROMIUM_DIR="/cygdrive/c/Program Files/chromium"
+elif [ -d "/cygdrive/c/Program Files/chromium-browser" ]; then
+	CHROMIUM_DIR="/cygdrive/c/Program Files/chromium-browser"
+elif [ -d "/cygdrive/c/Program Files (x86)/chromium" ]; then
+	CHROMIUM_DIR="/cygdrive/c/Program Files (x86)/chromium"
+elif [ -d "/cygdrive/c/Program Files (x86)/chromium-browser" ]; then
+	CHROMIUM_DIR="/cygdrive/c/Program Files (x86)/chromium-browser"
+fi
+echo $CHROMIUM_DIR
 
